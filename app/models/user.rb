203d2f70d44b704753d539afab74b6ae352fc6ce
taka110-IT <onyourmark_aska@yahoo.co.jp>
2021-07-15 15:53:37 +0900

    ActiveStorageで画像アップロードを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validate :avatar_type
+
+  private
+
+  def avatar_type
+    return unless avatar.attached?
+    return if avatar.blob.content_type.in?(%(image/jpeg image/png image/gif))
+
+    errors.add(:avatar, 'はjpeg・png・gif形式いずれかでアップロードしてください')
+  end
 end
