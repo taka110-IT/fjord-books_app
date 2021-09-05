fb3a06e6c3f2f15d8a0890d1199708e3b3aa31a5
taka110-IT <onyourmark_aska@yahoo.co.jp>
2021-09-05 10:57:14 +0900

    モデルテスト実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user1 = User.create!(email: 'user1@example.com', password: 'password')
+    @report1 = Report.create!(user_id: @user1.id, title: 'report1_title', content: 'report1_content')
+  end
+
+  test 'report editable' do
+    user2 = User.create!(email: 'user2@example.com', password: 'password')
+    assert @report1.editable?(@user1)
+    assert_not @report1.editable?(user2)
+  end
+
+  test 'created_on check' do
+    today = Date.today
+    assert_equal today, @report1.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name_or_email" do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar', user.name_or_email
+  end
+
+  test 'user follow_unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    assert_not she.followed_by?(me)
+    me.follow(she)
+    assert me.following?(she)
+    assert she.followed_by?(me)
+
+    me.unfollow(she)
+    assert_not me.following?(she)
+    assert_not she.followed_by?(me)
+  end
 end
